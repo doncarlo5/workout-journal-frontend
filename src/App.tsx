e77219a10e4c172ce67103cd851188cb50f5b86d
doncarlos5 <pro.julien.thomas@gmail.com>
2024-02-12 11:40:10 +0100

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import HomePage from "./pages/home-page"
 import LoginPage from "./pages/login-page"
 import ProfilePage from "./pages/profile-page"
 import SignupPage from "./pages/signup-page"
+import IsAuthenticated from "./routing/is-authenticated"
+import IsNotAuthenticated from "./routing/is-not-authenticated"
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route element={<IsNotAuthenticated />}>
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+        <Route element={<IsAuthenticated />}>
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
       </Routes>
     </div>
   )
