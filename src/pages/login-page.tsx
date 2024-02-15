a0f821bb65e6d0b7a4642592dade210ebb61aae1
doncarlos5 <pro.julien.thomas@gmail.com>
2024-02-15 13:28:43 +0100

    Update login page header to include an emoji

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -57,7 +57,7 @@ const LoginPage = () => {
       <form onSubmit={handleSubmit}>
         <div className="mx-auto max-w-sm space-y-6">
           <div className="space-y-2 text-center">
-            <h1 className="text-3xl font-bold">Se connecter</h1>
+            <h1 className="text-3xl font-bold">Se connecter 👤</h1>
             <p className="text-gray-500 dark:text-gray-400">Connecte toi avec tes informations</p>
           </div>
           <div className="space-y-4">
