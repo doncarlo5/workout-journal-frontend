11ac417ae8fc0a0ca8d254f8ec474e96f95b3493
doncarlos5 <pro.julien.thomas@gmail.com>
2024-03-29 14:40:47 +0100

    Add ErrorBoundary and SessionsList components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom"
 
 import { ThemeProvider } from "./components/theme-provider"
 import DoExercisePage from "./pages/do-exercise-page"
+import ErrorBoundary from "./pages/error-boundary"
 import ExercicesList from "./pages/exercises-list"
 import HomePage from "./pages/home-page"
 import LoginPage from "./pages/login-page"
 import NotFoundPage from "./pages/no-found-page"
 import OneExercise from "./pages/one-exercise"
+import SessionsList from "./pages/sessions-list"
 import SettingsPage from "./pages/settings-page"
 import SignupPage from "./pages/signup-page"
 import WelcomePage from "./pages/welcome-page"
@@ -17,25 +19,28 @@ import IsNotAuthenticated from "./routing/is-not-authenticated"
 function App() {
   return (
     <div className="">
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Routes>
-          <Route element={<IsAuthenticated redirect={"/welcome"} />}>
-            <Route path="/" element={<HomePage />} />
-          </Route>
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route element={<IsNotAuthenticated />}>
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Route>
-          <Route element={<IsAuthenticated redirect={"/"} />}>
-            <Route path="/exercises-list" element={<ExercicesList />} />
-            <Route path="/exercises-list/:exerciseId" element={<OneExercise />} />
-            <Route path="/do-exercise" element={<DoExercisePage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </ThemeProvider>
+      <ErrorBoundary>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <Routes>
+            <Route element={<IsNotAuthenticated />}>
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Route>
+            <Route element={<IsAuthenticated redirect={"/welcome"} />}>
+              <Route path="/" element={<HomePage />} />
+            </Route>
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route element={<IsAuthenticated redirect={"/"} />}>
+              <Route path="/sessions-list" element={<SessionsList />} />
+              <Route path="/exercises-list" element={<ExercicesList />} />
+              <Route path="/exercises-list/:exerciseId" element={<OneExercise />} />
+              <Route path="/do-exercise" element={<DoExercisePage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </ThemeProvider>
+      </ErrorBoundary>
     </div>
   )
 }
