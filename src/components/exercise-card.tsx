4616aaec2ee29e5b506c46639c601333d9ae9d02
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-08 15:13:54 +0200

    change design

--- src/components/exercise-card.tsx
+++ src/components/exercise-card.tsx
@@ -15,75 +15,56 @@ function ExerciseCard({ exercise }: { exercise: any }) {
               {")"}
             </CardTitle>
           </CardHeader>
-          <CardContent className=" pb-4 pl-4 pr-4">
+          <CardContent className="">
             <div className="relative overflow-x-auto">
-              <div className="relative overflow-x-auto">
-                <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
-                  <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                      <th scope="col" className="px-3 py-1">
-                        SERIES
-                      </th>
-                      <th scope="col" className="px-3 py-1">
-                        REPS
-                      </th>
-                      <th scope="col" className="px-3 py-1">
-                        KG
-                      </th>
-                      <th scope="col" className="px-3 py-1">
-                        RANGE
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-                      <th
-                        scope="row"
-                        className="whitespace-nowrap px-3 text-xl font-thin text-gray-900 dark:text-white"
-                      >
-                        ①
-                      </th>
-                      <td className="bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text px-3 py-2 text-xl font-black text-transparent ">
-                        {exercise.rep[0]}
-                      </td>
-                      <td className="bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text px-3 py-2 text-xl font-black text-transparent ">
-                        {exercise.weight[0]}
-                      </td>
-                      <td className="px-3 py-2">{exercise.type.repRange1}</td>
-                    </tr>
-                    <tr className="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-                      <th
-                        scope="row"
-                        className="whitespace-nowrap px-3 text-xl font-thin text-gray-900 dark:text-white"
-                      >
-                        ②
-                      </th>
-                      <td className="bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text px-3 py-2 text-xl font-black text-transparent ">
-                        {exercise.rep[1]}
-                      </td>
-                      <td className="bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text px-3 py-2 text-xl font-black text-transparent ">
-                        {exercise.weight[1]}
-                      </td>
-                      <td className="px-3 py-2">{exercise.type.repRange2}</td>
-                    </tr>
-                    <tr className="border-b bg-white dark:border-gray-700 dark:bg-gray-800">
-                      <th
-                        scope="row"
-                        className="whitespace-nowrap px-3 text-xl font-thin text-gray-900 dark:text-white"
-                      >
-                        ③
-                      </th>
-                      <td className="bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text px-3 py-2 text-xl font-black text-transparent ">
-                        {exercise.rep[2]}
-                      </td>
-                      <td className="bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text px-3 py-2 text-xl font-black text-transparent ">
-                        {exercise.weight[2]}
-                      </td>
-                      <td className="px-3 py-2">{exercise.type.repRange3}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <table className="w-full rounded-md text-left">
+                <tbody className=" ">
+                  <tr className=" bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <th scope="row" className="whitespace-nowrap px-3 text-2xl font-thin text-gray-900 dark:text-white">
+                      ①
+                    </th>
+                    <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
+                      <span className="text-2xl font-bold">{exercise.rep[2]}</span>{" "}
+                      <span className="text-sm text-gray-700">REPS</span>{" "}
+                    </td>
+                    <td className=" px-3 py-2">
+                      <span className=" text-2xl font-bold">{exercise.weight[2]}</span>{" "}
+                      <span className="text-sm text-gray-700">KG</span>
+                    </td>
+                    <td className="px-3 py-2 font-light tracking-tighter text-gray-700">{exercise.type.repRange1}</td>
+                  </tr>
+
+                  <tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <th scope="row" className="whitespace-nowrap px-3 text-2xl font-thin text-gray-900 dark:text-white">
+                      ②
+                    </th>
+                    <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
+                      <span className="text-2xl font-bold">{exercise.rep[1]}</span>{" "}
+                      <span className="text-sm text-gray-700">REPS</span>{" "}
+                    </td>
+                    <td className=" px-3 py-2">
+                      <span className=" text-2xl font-bold">{exercise.weight[1]}</span>{" "}
+                      <span className="text-sm text-gray-700">KG</span>
+                    </td>
+                    <td className="px-3 py-2 font-light tracking-tighter text-gray-700">{exercise.type.repRange2}</td>
+                  </tr>
+
+                  <tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <th scope="row" className="whitespace-nowrap px-3 text-2xl font-thin text-gray-900 dark:text-white">
+                      ③
+                    </th>
+                    <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
+                      <span className="text-2xl font-bold">{exercise.rep[0]}</span>{" "}
+                      <span className="text-sm text-gray-700">REPS</span>{" "}
+                    </td>
+                    <td className=" px-3 py-2">
+                      <span className=" text-2xl font-bold">{exercise.weight[0]}</span>{" "}
+                      <span className="text-sm text-gray-700">KG</span>
+                    </td>
+                    <td className="px-3 py-2 font-light tracking-tighter text-gray-700">{exercise.type.repRange3}</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </CardContent>
         </Card>
