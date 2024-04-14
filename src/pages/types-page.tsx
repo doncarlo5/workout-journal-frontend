e05db8756e0bff95b4fb3263738fbbc2762b36e0
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-14 11:55:22 +0200

    Add TypesPage

---
+++ src/pages/types-page.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+import myApi from "@/lib/api-handler"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Navbar } from "@/components/navbar"
+
+export function TypesPage() {
+  const [type, setType] = useState([] as any[])
+
+  const fetchTypes = async () => {
+    try {
+      const response = await myApi.get("/exercise-type")
+      console.log("👋 response data", response.data)
+      setType(response.data)
+    } catch (error) {
+      console.error("Fetch error: ", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTypes()
+  }, [])
+
+  const handleDelete = async (id: string) => {
+    try {
+      const response = await myApi.delete(`/exercise-type/${id}`)
+      console.log(response)
+      fetchTypes()
+    } catch (error) {
+      console.error("Fetch error: ", error)
+    }
+  }
+
+  const formatDate = (dateString: string) => {
+    return new Date(dateString).toLocaleDateString()
+  }
+
+  return (
+    <div className="flex h-screen flex-col">
+      <Navbar />
+      {type.length !== 0 && (
+        <main className="flex flex-1 flex-col items-center justify-center">
+          <div className="space-y-2 text-center">
+            <h1 className="mb-5 mt-5 text-3xl font-bold">Mes types d'exercices</h1>
+          </div>
+          <div>
+            <Table>
+              <TableCaption>Liste de tes types d'exercices</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead></TableHead>
+                  <TableHead></TableHead>
+                  <TableHead>Nom</TableHead>
+                  <TableHead>Conseil</TableHead>
+                  <TableHead>{`Timer (en s)`}</TableHead>
+                  <TableHead>Rep Range 1</TableHead>
+                  <TableHead>Rep Range 2</TableHead>
+                  <TableHead>Rep Range 3</TableHead>
+                  <TableHead>Type de séance</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {type.map((type) => (
+                  <TableRow key={type._id}>
+                    <TableCell>
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button variant="ghost">✕</Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>Es-tu sûr de vouloir supprimer ton exercice ?</AlertDialogTitle>
+                            <AlertDialogDescription>Cette action est irréversible.</AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Conserver</AlertDialogCancel>
+                            <AlertDialogAction asChild onClick={() => handleDelete(type._id)}>
+                              <Button variant="destructive">Confirmer</Button>
+                            </AlertDialogAction>{" "}
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </TableCell>
+                    <TableCell>
+                      <Link to={`/exercise-type/${type._id}`} key={type._id}>
+                        <Button variant="ghost">✍️</Button>
+                      </Link>
+                    </TableCell>
+                    <TableCell>{type.name}</TableCell>
+                    <TableCell>{type.advice}</TableCell>
+                    <TableCell>{type.timer}</TableCell>
+                    <TableCell>{type.repRange1}</TableCell>
+                    <TableCell>{type.repRange2}</TableCell>
+                    <TableCell>{type.repRange3}</TableCell>
+                    <TableCell>{type.type_session}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </main>
+      )}
+      {type.length === 0 && (
+        <main className="flex flex-1 items-center justify-center">
+          <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
+            <div className="text-center">
+              <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl">
+                Tu n'as pas encore de type exercice.
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <Link className="w-[150px]" to="/do-exercise">
+                <Button className="w-full">Créer un exercice type</Button>
+              </Link>
+            </div>
+          </div>
+        </main>
+      )}
+    </div>
+  )
+}
+
+export default TypesPage
