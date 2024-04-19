cd29f880899785e8c15b49a0bb80cb7b925c3162
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-19 11:42:20 +0200

    Update ExerciseCard component to display exercise details and comments

--- src/components/exercise-card.tsx
+++ src/components/exercise-card.tsx
@@ -1,6 +1,7 @@
 import { ChatBubbleIcon, TextAlignRightIcon } from "@radix-ui/react-icons"
 import { Link } from "react-router-dom"
 
+import { Badge } from "./ui/badge"
 import { Button } from "./ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
@@ -8,7 +9,70 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/t
 function ExerciseCard({ exercise }: { exercise: any }) {
   return (
     <Link to={`/exercises/${exercise._id}`}>
-      <Card className="transition ease-in-out hover:-translate-y-1 hover:shadow-lg">
+      <Card className="w-full max-w-sm">
+        <CardHeader className="px-4 pb-0 pt-4">
+          <CardTitle className="text-lg">{exercise.type.name}</CardTitle>
+        </CardHeader>
+        <CardContent className="ml-3 mt-2 px-4 pb-4">
+          <div className="mb-4 flex items-center gap-4">
+            <div className="flex w-28 items-baseline gap-1 text-2xl font-bold">
+              {exercise.rep[0]}
+              <span className="text-xs font-normal text-gray-700"> REPS</span>
+            </div>
+            <div className="flex w-40 items-baseline gap-1 text-2xl font-bold">
+              {exercise.weight[0]}
+              <span className="text-xs font-normal text-gray-700"> KG</span>
+            </div>
+            <div className="flex w-40 items-center text-sm font-medium">
+              <Badge className=" min-h-9 min-w-28 select-none justify-center text-lg font-light " variant="secondary">
+                {exercise.type.repRange1}
+              </Badge>
+            </div>
+          </div>
+
+          <div className="mb-4 flex items-center gap-4">
+            <div className="flex w-28 items-baseline gap-1 text-2xl font-bold">
+              {exercise.rep[1]}
+              <span className="text-xs font-normal text-gray-700"> REPS</span>
+            </div>
+            <div className="flex w-40 items-baseline gap-1 text-2xl font-bold">
+              {exercise.weight[1]}
+              <span className="text-xs font-normal text-gray-700"> KG</span>
+            </div>
+            <div className="flex w-40 items-center text-sm font-medium">
+              <Badge className=" min-h-9 min-w-28 select-none justify-center text-lg font-light " variant="secondary">
+                {exercise.type.repRange2}
+              </Badge>
+            </div>
+          </div>
+
+          <div className="mb-4 flex items-center gap-4">
+            <div className="flex w-28 items-baseline gap-1 text-2xl font-bold">
+              {exercise.rep[2]}
+              <span className="text-xs font-normal text-gray-700"> REPS</span>
+            </div>
+            <div className="flex w-40 items-baseline gap-1 text-2xl font-bold">
+              {exercise.weight[2]}
+              <span className="text-xs font-normal text-gray-700"> KG</span>
+            </div>
+            <div className="flex w-40 items-center text-sm font-medium">
+              <Badge className=" min-h-9 min-w-28 select-none justify-center text-lg font-light " variant="secondary">
+                {exercise.type.repRange3}
+              </Badge>
+            </div>
+          </div>
+        </CardContent>
+        {exercise.comment && (
+          <CardContent className="px-4 pb-4">
+            <div className="flex items-center gap-2">
+              <ChatBubbleIcon className="h-6 w-6" />
+              <CardDescription>{exercise.comment}</CardDescription>
+            </div>
+          </CardContent>
+        )}
+      </Card>
+
+      {/* <Card className="transition ease-in-out hover:-translate-y-1 hover:shadow-lg">
         <CardHeader className="  flex flex-row justify-between px-4 py-2 ">
           <CardTitle className="flex items-center  ">{exercise.type.name}</CardTitle>
           {exercise.comment && (
@@ -24,14 +88,11 @@ function ExerciseCard({ exercise }: { exercise: any }) {
             </TooltipProvider>
           )}
         </CardHeader>
-        <CardContent className="">
+        <CardContent className=" m-1 p-1">
           <div className="relative overflow-x-auto">
             <table className="w-full rounded-md text-left">
               <tbody className=" ">
                 <tr className=" bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <th scope="row" className="whitespace-nowrap px-3 text-2xl font-thin text-gray-900 dark:text-white">
-                    ①
-                  </th>
                   <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
                     <span className="text-2xl font-bold">{exercise.rep[0]}</span>{" "}
                     <span className="text-sm text-gray-700">REPS</span>{" "}
@@ -40,17 +101,12 @@ function ExerciseCard({ exercise }: { exercise: any }) {
                     <span className=" text-2xl font-bold">{exercise.weight[0]}</span>{" "}
                     <span className="text-sm text-gray-700">KG</span>
                   </td>
-                  <td className="px-3 py-2 font-light tracking-tighter text-gray-700">
-                    {"("}
+                  <td className=" rounded-xl bg-gray-300 px-3 py-2 text-center font-light tracking-tighter text-gray-700">
                     {exercise.type.repRange1}
-                    {")"}
                   </td>
                 </tr>
 
                 <tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <th scope="row" className="whitespace-nowrap px-3 text-2xl font-thin text-gray-900 dark:text-white">
-                    ②
-                  </th>
                   <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
                     <span className="text-2xl font-bold">{exercise.rep[1]}</span>{" "}
                     <span className="text-sm text-gray-700">REPS</span>{" "}
@@ -67,9 +123,6 @@ function ExerciseCard({ exercise }: { exercise: any }) {
                 </tr>
 
                 <tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <th scope="row" className="whitespace-nowrap px-3 text-2xl font-thin text-gray-900 dark:text-white">
-                    ③
-                  </th>
                   <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
                     <span className="text-2xl font-bold">{exercise.rep[2]}</span>{" "}
                     <span className="text-sm text-gray-700">REPS</span>{" "}
@@ -88,7 +141,7 @@ function ExerciseCard({ exercise }: { exercise: any }) {
             </table>
           </div>
         </CardContent>
-      </Card>
+      </Card> */}
     </Link>
   )
 }
