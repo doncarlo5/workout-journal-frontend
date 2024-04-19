09116fbb87ec10ec746fdf029b48e1f6b4ddf843
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-19 10:13:42 +0200

    Add NewSessionButton component to SessionsList page

--- src/components/new-session-button.tsx
+++ src/components/new-session-button.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { PlusIcon } from "@radix-ui/react-icons"
 import { useNavigate, useParams } from "react-router-dom"
 
@@ -6,6 +6,7 @@ import myApi from "../lib/api-handler"
 import IconChest from "./chest-icon"
 import IconLegs from "./legs-icon"
 import { Button } from "./ui/button"
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog"
 import {
   Drawer,
   DrawerClose,
@@ -16,9 +17,14 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "./ui/drawer"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
 
 function NewSessionButton() {
-  const [weight, setWeight] = React.useState(null)
+  const [weight, setWeight] = React.useState("")
+  const [showDialog, setShowDialog] = useState(false)
+  const [typeSession, setTypeSession] = useState("")
+
   const navigate = useNavigate()
 
   // Fetch the last exercise user to get the last weight
@@ -28,15 +34,22 @@ function NewSessionButton() {
   }, [])
 
   const fetchLastSessionUser = async () => {
-    console.log("fetchLastSessionUser")
-    const response = await myApi.get(`/sessions?limit=1&sort=-createdAt`)
-    setWeight(response.data[0]?.body_weight)
-    console.log("LastSessionUser is:", response.data)
+    try {
+      const response = await myApi.get(`/sessions?limit=1&sort=-createdAt`)
+      if (response.data && response.data.length > 0) {
+        setWeight(response.data[0].body_weight)
+      } else {
+        setShowDialog(true)
+      }
+    } catch (error) {
+      console.error("Error fetching last session:", error)
+    }
   }
 
   const handleCreateSession = async (e: React.FormEvent, userChoice: string) => {
     e.preventDefault()
     try {
+      setTypeSession(userChoice)
       const response = await myApi.post("/sessions", {
         date_session: new Date(),
         type_session: userChoice,
@@ -54,61 +67,79 @@ function NewSessionButton() {
   }
 
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
-        <Button className=" rounded-2xl" variant="outline">
-          <PlusIcon />
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mx-auto w-full max-w-sm">
-          <DrawerHeader className=" text-left">
-            <DrawerTitle>Nouvelle séance</DrawerTitle>
-            <DrawerDescription>Créer une séance pour réaliser tes exercises.</DrawerDescription>
-          </DrawerHeader>
-          <div className="p-4 pb-0">
-            <div className="flex items-center justify-between space-x-2">
-              <Button
-                onClick={(e) => handleCreateSession(e, "Upper A")}
-                variant="outline"
-                size="icon"
-                className="flex h-24 w-24 flex-col rounded-md"
-              >
-                <p className="mb-1 mt-4 text-lg">Upper A</p>
-                <IconChest className="mt-3" />
-                <span className="sr-only">Upper A</span>
-              </Button>
-              <Button
-                onClick={(e) => handleCreateSession(e, "Lower")}
-                variant="outline"
-                size="icon"
-                className="flex h-24 w-24 flex-col rounded-md"
-              >
-                <p className="mb-1 mt-4 text-lg">Lower</p>
-                <IconLegs className="mt-3" />
-                <span className="sr-only">Lower</span>
-              </Button>
-              <Button
-                onClick={(e) => handleCreateSession(e, "Upper B")}
-                variant="outline"
-                size="icon"
-                className="flex h-24 w-24 flex-col rounded-md"
-              >
-                <p className="mb-1 mt-4 text-lg">Upper B</p>
-                <IconChest className="mt-3" />
-                <span className="sr-only">Upper B</span>
-              </Button>
+    <>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button className=" rounded-2xl" variant="outline">
+            <PlusIcon />
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mx-auto w-full max-w-sm">
+            <DrawerHeader className=" text-left">
+              <DrawerTitle>Nouvelle séance</DrawerTitle>
+              <DrawerDescription>Créer une séance pour réaliser tes exercises.</DrawerDescription>
+            </DrawerHeader>
+            <div className="p-4 pb-0">
+              <div className="flex items-center justify-between space-x-2">
+                <Button
+                  onClick={(e) => handleCreateSession(e, "Upper A")}
+                  variant="outline"
+                  size="icon"
+                  className="flex h-24 w-24 flex-col rounded-md"
+                >
+                  <p className="mb-1 mt-4 text-lg">Upper A</p>
+                  <IconChest className="mt-3" />
+                  <span className="sr-only">Upper A</span>
+                </Button>
+                <Button
+                  onClick={(e) => handleCreateSession(e, "Lower")}
+                  variant="outline"
+                  size="icon"
+                  className="flex h-24 w-24 flex-col rounded-md"
+                >
+                  <p className="mb-1 mt-4 text-lg">Lower</p>
+                  <IconLegs className="mt-3" />
+                  <span className="sr-only">Lower</span>
+                </Button>
+                <Button
+                  onClick={(e) => handleCreateSession(e, "Upper B")}
+                  variant="outline"
+                  size="icon"
+                  className="flex h-24 w-24 flex-col rounded-md"
+                >
+                  <p className="mb-1 mt-4 text-lg">Upper B</p>
+                  <IconChest className="mt-3" />
+                  <span className="sr-only">Upper B</span>
+                </Button>
+              </div>
+              <div className="mt-3 h-2"></div>
             </div>
-            <div className="mt-3 h-2"></div>
           </div>
-          <DrawerFooter>
-            <DrawerClose asChild>
-              <Button variant="outline">Annuler</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </div>
-      </DrawerContent>
-    </Drawer>
+        </DrawerContent>
+        {showDialog && (
+          <div>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className=" mb-5">Veuillez renseigner votre poids {`(en kg)`}</DialogTitle>
+                <DialogDescription>
+                  <Input
+                    className=" w-1/4"
+                    type="number"
+                    id="body_weight"
+                    value={weight}
+                    onChange={(e) => setWeight(e.target.value)}
+                  />
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button onClick={() => setShowDialog(false)}>Valider</Button>
+              </DialogFooter>
+            </DialogContent>
+          </div>
+        )}
+      </Drawer>
+    </>
   )
 }
 
