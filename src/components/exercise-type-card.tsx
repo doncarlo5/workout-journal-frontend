53222e8c4d2cfcf818218c595dcdd43e396365f7
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-22 09:15:14 +0200

    Add ExerciseTypeCard component to display exercise type details in profile page

---
+++ src/components/exercise-type-card.tsx
@@ -0,0 +1,38 @@
+import { ChatBubbleIcon, TextAlignRightIcon } from "@radix-ui/react-icons"
+import { LucideInfo, LucideMessageSquareText, LucideTimer } from "lucide-react"
+import { Link } from "react-router-dom"
+
+import { Badge } from "./ui/badge"
+import { Button } from "./ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Label } from "./ui/label"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
+
+function ExerciseTypeCard({ exerciseType }: { exerciseType: any }) {
+  return (
+    <Link to={`/profile/type/${exerciseType._id}`}>
+      <Card className="h-full w-full max-w-md transform-gpu">
+        <CardHeader className=" p-4">
+          <CardTitle>{exerciseType.name}</CardTitle>
+          <CardDescription>{exerciseType.type_session}</CardDescription>
+        </CardHeader>
+        <CardContent className=" p-4">
+          <div className=" flex justify-between">
+            <div className="flex items-baseline gap-1 ">
+              <LucideTimer size={16} className="text-gray-500 dark:text-gray-400" />
+              <div>
+                <p className="text-gray-500">{exerciseType.timer}s</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-1 ">
+              <LucideInfo size={16} className={exerciseType.advice ? "text-slate-100" : "text-slate-900"} />
+            </div>
+          </div>
+          <div></div>
+        </CardContent>
+      </Card>
+    </Link>
+  )
+}
+
+export default ExerciseTypeCard
