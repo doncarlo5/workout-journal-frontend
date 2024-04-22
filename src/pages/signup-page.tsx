b77183b83288b85e1f328e1a3f30ca4cb10b4b44
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-22 11:49:27 +0200

    Update signup-page.tsx to improve UI layout and add profile section

--- src/pages/signup-page.tsx
+++ src/pages/signup-page.tsx
@@ -6,22 +6,25 @@ import SignupComponent from "../components/signup-component"
 
 const SignupPage = () => {
   return (
-    <div className=" m-auto ">
-      <div className="mb-10 flex flex-col">
-        <Navbar />
-      </div>
-      <Tabs defaultValue="signup" className="relative w-[400px]  ">
-        <TabsList className="grid w-full max-w-2xl grid-cols-2 rounded-xl ">
-          <TabsTrigger value="signup">S'inscrire</TabsTrigger>
-          <TabsTrigger value="signin">Se connecter</TabsTrigger>
-        </TabsList>
-        <TabsContent value="signup">
-          <SignupComponent />
-        </TabsContent>
-        <TabsContent value="signin">
-          <LoginComponent />
-        </TabsContent>
-      </Tabs>
+    <div className=" mx-auto ">
+      <Navbar />
+      <main className="flex flex-1 flex-col items-center justify-center">
+        <div className="space-y-2 text-center">
+          <h1 className="mb-5 mt-5 text-3xl font-bold">Profile</h1>
+        </div>
+        <Tabs defaultValue="signup" className="relative w-[400px]  ">
+          <TabsList className="grid w-full max-w-2xl grid-cols-2 rounded-xl ">
+            <TabsTrigger value="signup">S'inscrire</TabsTrigger>
+            <TabsTrigger value="signin">Se connecter</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signup">
+            <SignupComponent />
+          </TabsContent>
+          <TabsContent value="signin">
+            <LoginComponent />
+          </TabsContent>
+        </Tabs>
+      </main>
     </div>
   )
 }
