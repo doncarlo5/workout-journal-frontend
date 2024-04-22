b1d1125bc028174f87b91f4fcb7adb8b7cdcdd7c
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-22 09:14:28 +0200

    Update routing in App.tsx to include new history pages and profile pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@
 import { Route, Routes } from "react-router-dom"
 
 import { ThemeProvider } from "./components/theme-provider"
-import ChartsPage from "./pages/charts-page"
 import DoExercisePage from "./pages/do-exercise-page"
 import ErrorBoundary from "./pages/error-boundary"
 import ExercicesList from "./pages/exercises-list"
+import SessionsList, { HistoryPage } from "./pages/history-page"
 import HomePage from "./pages/home-page"
 import LoginPage from "./pages/login-page"
 import NewType from "./pages/new-type"
@@ -14,8 +14,9 @@ import OneExercise from "./pages/one-exercise"
 import OneSession from "./pages/one-session"
 import OneType from "./pages/one-type"
 import ProfilePage from "./pages/profile-page"
-import SessionsList from "./pages/sessions-list"
 import SignupPage from "./pages/signup-page"
+import ChartsPage from "./pages/stats-page"
+import StatsPage from "./pages/stats-page"
 import TypesList from "./pages/types-list"
 import WelcomePage from "./pages/welcome-page"
 import IsAuthenticated from "./routing/is-authenticated"
@@ -23,7 +24,7 @@ import IsNotAuthenticated from "./routing/is-not-authenticated"
 
 function App() {
   return (
-    <div className="flex h-dvh w-screen">
+    <div className="">
       <ErrorBoundary>
         <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
           <Routes>
@@ -36,15 +37,16 @@ function App() {
             </Route>
             <Route path="/welcome" element={<WelcomePage />} />
             <Route element={<IsAuthenticated redirect={"/"} />}>
-              <Route path="/sessions" element={<SessionsList />} />
-              <Route path="/sessions/:sessionId" element={<OneSession />} />
-              <Route path="/sessions/:sessionId/do-exercise" element={<DoExercisePage />} />
-              <Route path="/exercises" element={<ExercicesList />} />
-              <Route path="/exercises/:exerciseId" element={<OneExercise />} />
-              <Route path="/types" element={<TypesList />} />
-              <Route path="/types/:typeId" element={<OneType />} />
-              <Route path="/types/new-type" element={<NewType />} />
-              <Route path="/charts" element={<ChartsPage />} />
+              <Route path="/history" element={<HistoryPage />} />
+              {/* <Route path="/sessions" element={<SessionsList />} /> */}
+              <Route path="/history/exercise/:exerciseId" element={<OneExercise />} />
+              <Route path="/history/session/:sessionId" element={<OneSession />} />
+              <Route path="/history/:sessionId/do-exercise" element={<DoExercisePage />} />
+              {/* <Route path="/exercises" element={<ExercicesList />} /> */}
+              <Route path="/profile/type" element={<TypesList />} />
+              <Route path="/profile/type/:typeId" element={<OneType />} />
+              <Route path="/profile/type/new-type" element={<NewType />} />
+              <Route path="/stats" element={<StatsPage />} />
               <Route path="/profile" element={<ProfilePage />} />
             </Route>
             <Route path="*" element={<NotFoundPage />} />
