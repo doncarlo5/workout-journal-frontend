3ab10a89d29ade1d2be07012fdfbd82bdb0ead01
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-26 15:48:36 +0200

    Refactor ModeToggle component to use Button component for theme toggle

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -2,6 +2,8 @@ import { Moon, Sun } from "lucide-react"
 
 import { useTheme } from "@/components/theme-provider"
 
+import { Button } from "./ui/button"
+
 export function ModeToggle() {
   const { theme, setTheme } = useTheme()
 
@@ -10,22 +12,19 @@ export function ModeToggle() {
   }
 
   return (
-    <div
-      className=" flex cursor-pointer items-center justify-center rounded-md bg-slate-100 px-6 py-2 text-center hover:text-teal-500 focus:text-teal-500 dark:bg-gray-800 dark:text-gray-100 dark:hover:text-teal-500 dark:focus:text-teal-500"
-      onClick={toggleTheme}
-    >
+    <Button variant={"outline"} onClick={toggleTheme}>
       {theme === "light" ? (
         <>
-          <Moon className=" inline-block transition-all dark:rotate-0 dark:scale-100" size={24} />
+          <Moon className=" inline-block transition-all dark:rotate-0 dark:scale-100" size={16} />
           {/* <span className="tab tab-home block select-none text-xs">Dark</span> */}
         </>
       ) : (
         <>
-          <Sun className=" inline-block " size={24} />
+          <Sun className=" inline-block " size={16} />
           {/* <span className="tab tab-home block select-none text-xs  ">Light</span> */}
         </>
       )}
       <span className="sr-only">Toggle theme</span>
-    </div>
+    </Button>
   )
 }
