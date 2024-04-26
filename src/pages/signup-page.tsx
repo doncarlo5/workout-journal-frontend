4d23043d6f92d1643fcf08bc6b723dd65d02a9d0
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-26 12:00:20 +0200

    Update signup-page.tsx to improve UI layout and add login and signup tabs

--- src/pages/signup-page.tsx
+++ src/pages/signup-page.tsx
@@ -9,22 +9,19 @@ const SignupPage = () => {
     <div className=" mx-auto">
       <Navbar />
       <main className="flex flex-1 flex-col items-center justify-center">
-        <div className="space-y-2 text-center">
-          <h1 className="mb-5 mt-5 text-3xl font-bold">Profile</h1>
-        </div>
         <Tabs
-          defaultValue="signup"
-          className="container flex flex-col items-center justify-center gap-4 px-10  md:px-6"
+          defaultValue="login"
+          className="container flex flex-col items-center justify-center gap-4 px-10  pt-20 md:px-6"
         >
           <TabsList className="grid w-full max-w-2xl grid-cols-2 rounded-xl ">
-            <TabsTrigger value="signup">S'inscrire</TabsTrigger>
-            <TabsTrigger value="signin">Se connecter</TabsTrigger>
+            <TabsTrigger value="login">Connexion</TabsTrigger>
+            <TabsTrigger value="signup">Inscription</TabsTrigger>
           </TabsList>
+          <TabsContent value="login">
+            <LoginComponent />
+          </TabsContent>
           <TabsContent value="signup">
             <SignupComponent />
-          </TabsContent>
-          <TabsContent value="signin">
-            <LoginComponent />
           </TabsContent>
         </Tabs>
       </main>
