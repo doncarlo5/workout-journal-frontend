654e0ed6dea851b76eddc2b4808094b855d5c38c
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-27 10:53:44 +0200

    Update stats-page.tsx to add import statements for LucideReact and react-router-dom, and improve UI layout

--- src/pages/stats-page.tsx
+++ src/pages/stats-page.tsx
@@ -1,3 +1,7 @@
+import { ChevronLeft } from "lucide-react"
+import { Link } from "react-router-dom"
+
+import { Button } from "@/components/ui/button"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import BodyWeightChart from "@/components/body-weight-chart"
 import ExerciseChart from "@/components/exercise-chart"
@@ -5,24 +9,30 @@ import { Navbar } from "@/components/navbar"
 
 function StatsPage() {
   return (
-    <div className=" m-auto">
+    <div className="mx-auto max-w-sm space-y-6 p-4">
       <div className="">
         <Navbar />
-
         <main className="flex flex-1 flex-col items-center justify-center">
-          <div className="space-y-2 text-center">
-            <h1 className="mb-5 mt-5 text-3xl font-bold">Statistiques</h1>
+          <div className="flex items-center space-y-2 text-left">
+            <Link to="/profile">
+              <Button variant="outline" size="icon">
+                <ChevronLeft className="h-4 w-4" />
+              </Button>
+            </Link>
+            <div>
+              <h1 className="ml-5 py-5 text-3xl font-medium">Statistiques</h1>
+            </div>
           </div>
 
           <Tabs defaultValue="weight_body" className="size-80">
-            <TabsList className="grid w-full grid-cols-2">
+            <TabsList className="my-5 grid w-full grid-cols-2">
               <TabsTrigger value="weight_body">Poids du corps</TabsTrigger>
               <TabsTrigger value="exercise">Exercices</TabsTrigger>
             </TabsList>
-            <TabsContent asChild className=" h-64" value="weight_body">
+            <TabsContent asChild className="h-64" value="weight_body">
               <BodyWeightChart />
             </TabsContent>
-            <TabsContent asChild className=" h-64" value="exercise">
+            <TabsContent asChild className="h-64" value="exercise">
               <ExerciseChart />
             </TabsContent>
           </Tabs>
