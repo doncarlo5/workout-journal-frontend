4f28f365d3caa03d903e62c2ab2d8234e88df893
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-27 10:54:07 +0200

    Update is-not-authenticated.tsx to add loading spinner and improve UI layout

--- src/routing/is-not-authenticated.tsx
+++ src/routing/is-not-authenticated.tsx
@@ -1,11 +1,18 @@
 import useAuth from "@/context/use-auth"
+import { LucideLoader2 } from "lucide-react"
 import { Navigate, Outlet } from "react-router-dom"
 
 const IsNotAuthenticated = () => {
   const { isLoggedIn, isLoading } = useAuth()
 
   if (isLoading) {
-    return <p>Loading</p>
+    return (
+      <div className="container flex flex-col items-center justify-center p-20">
+        <div className="">
+          <LucideLoader2 className=" animate-spin " size={32} />
+        </div>
+      </div>
+    )
   }
   if (isLoggedIn) {
     return <Navigate to="/" />
