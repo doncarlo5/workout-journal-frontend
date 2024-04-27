31f8981a1cc65f33c228ed263961b15cf24e3062
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-27 10:53:40 +0200

    Update profile-page.tsx to improve UI layout and remove unused components

--- src/pages/profile-page.tsx
+++ src/pages/profile-page.tsx
@@ -1,59 +1,14 @@
 import { useState } from "react"
 import useAuth from "@/context/use-auth"
-import { LucideLogOut, Trophy } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useToast } from "@/components/ui/use-toast"
-import { ModeToggle } from "@/components/mode-toggle"
+import DashboardComponent from "@/components/dashboard-component"
 import { Navbar } from "@/components/navbar"
-import TrophyComponent from "@/components/trophy-component"
-import TypeComponent from "@/components/type-component"
 
 import myApi from "../lib/api-handler"
 
 const ProfilePage = () => {
-  const { toast } = useToast()
-  const { user, handleLogout } = useAuth()
-
-  const [formState, setFormState] = useState({
-    firstName: user?.firstName || "",
-    lastName: user?.lastName || "",
-    email: user?.email || "",
-  })
-  const [error, setError] = useState("")
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const { target } = event
-    if (target instanceof HTMLInputElement) {
-      const key = target.id
-      const value = target.value
-      setFormState({ ...formState, [key]: value })
-    }
-  }
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    try {
-      const response = await myApi.patch("/api/auth/settings", formState)
-      toast({
-        title: "Profil mis à jour!",
-      })
-
-      console.log(response)
-    } catch (error: any) {
-      toast({
-        variant: "destructive",
-        title: "Une erreur est survenue! ❌",
-      })
-      setError(error.response.data.message)
-      setTimeout(() => {
-        setError("")
-      }, 3000)
-    }
-  }
+  const { user } = useAuth()
 
   return (
     <div className=" mx-auto">
@@ -61,85 +16,10 @@ const ProfilePage = () => {
 
       <main className="flex flex-1 flex-col items-center justify-center">
         <div className="space-y-2 text-center">
-          <h1 className="mb-5 mt-5 text-3xl font-bold">Profile</h1>
+          <h1 className="mb-5 mt-5 text-3xl font-bold">Profil de {user?.firstName}</h1>
         </div>
-        <Tabs defaultValue="info" className=" w-80 pb-16">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="info">Informations</TabsTrigger>
-            <TabsTrigger value="trophy">Trophées</TabsTrigger>
-          </TabsList>
-          <TabsContent asChild className="" value="info">
-            <div className=" flex flex-1 flex-col items-center justify-center">
-              <div className="space-y-2 text-center">
-                <h1 className="mb-5 mt-5 text-xl font-bold">Modifier son profil</h1>
-              </div>
-              <div className=" ">
-                <form onSubmit={handleSubmit}>
-                  <div className="space-y-4">
-                    <div className="grid grid-cols-2 gap-4">
-                      <div className="space-y-2">
-                        <Label htmlFor="firstName">Prénom</Label>
-                        <Input
-                          id="firstName"
-                          placeholder="Prénom"
-                          value={formState.firstName}
-                          onChange={handleChange}
-                          required
-                          type="text"
-                        />
-                      </div>
-                      <div className="space-y-2">
-                        <Label htmlFor="lastName">Nom</Label>
-                        <Input
-                          id="lastName"
-                          placeholder="Nom"
-                          value={formState.lastName}
-                          onChange={handleChange}
-                          required
-                          type="text"
-                        />
-                      </div>
-                    </div>
-                    <div className="space-y-2">
-                      <Label htmlFor="email">Email</Label>
-                      <Input
-                        id="email"
-                        placeholder="Email"
-                        value={formState.email}
-                        onChange={handleChange}
-                        required
-                        type="email"
-                      />
-                    </div>
-                    <Button
-                      className="mt-4 w-full"
-                      type="submit"
-                      onClick={() => {
-                        toast({
-                          description: "Ton profil a bien été mis à jour! ✅",
-                        })
-                      }}
-                    >
-                      Mettre à jour
-                    </Button>
-                  </div>
-                </form>
-                <div className=" m-8 flex justify-center">
-                  <ModeToggle />
-                </div>
-                <div className=" flex justify-center">
-                  <Button onClick={handleLogout} variant={"outline"} className=" flex  ">
-                    <LucideLogOut className=" " size={16} />
-                  </Button>
-                </div>
-                {error && <p>{error}</p>}
-              </div>
-            </div>
-          </TabsContent>
-          <TabsContent asChild className="" value="trophy">
-            <TrophyComponent />
-          </TabsContent>
-        </Tabs>
+
+        <DashboardComponent />
       </main>
     </div>
   )
