7f667802e713f686f545de95ea9d54f96d1af071
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-27 10:53:33 +0200

    Update TypeComponent to add "Ajouter un exercice" button and improve UI layout

--- src/components/type-component.tsx
+++ src/components/type-component.tsx
@@ -29,9 +29,18 @@ export function TypeComponent() {
   return (
     <>
       {!isLoading && exerciseType.length !== 0 && (
-        <main className="mt-2 flex flex-1 flex-col items-center justify-center">
+        <main className="mt-2 flex flex-1 flex-col items-center justify-center pb-20">
           <div className="space-y-2 text-center">
             <h1 className="mb-5 mt-5 text-xl font-bold">Mes types d'exercices</h1>
+          </div>
+          <div className="px-4">
+            <Link to={`/type/new-type`}>
+              <Button className=" w-full">
+                {" "}
+                <LucidePlusCircle className=" mr-2 size-5" />
+                Ajouter un exercice{" "}
+              </Button>
+            </Link>
           </div>
           <div>
             <div className=" col-span-2 ">
@@ -52,15 +61,6 @@ export function TypeComponent() {
                       ))
                     )}
                   </div>
-                  <div className="px-4">
-                    <Link to={`/type/new-type`}>
-                      <Button className=" w-full">
-                        {" "}
-                        <LucidePlusCircle className=" mr-2 size-5" />
-                        Ajouter un exercice{" "}
-                      </Button>
-                    </Link>
-                  </div>
                 </div>
               </div>
             </div>
@@ -68,7 +68,7 @@ export function TypeComponent() {
         </main>
       )}
       {!isLoading && exerciseType.length === 0 && (
-        <main className="flex flex-1 items-center justify-center">
+        <main className="flex flex-1 items-center justify-center pb-5">
           <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
             <div className="text-center">
               <div className="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
