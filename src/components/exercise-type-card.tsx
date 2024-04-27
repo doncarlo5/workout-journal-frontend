fe488f086755e5e78867dcd7b8e42840fca6c69e
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-27 10:52:58 +0200

    Update ExerciseTypeCard component to improve UI layout and add conditional rendering for exercise advice

--- src/components/exercise-type-card.tsx
+++ src/components/exercise-type-card.tsx
@@ -1,28 +1,34 @@
-import { LucideClock4, LucideInfo, LucideTag, LucideTimer } from "lucide-react"
+import { LucideClock4, LucideInfo, LucideTag } from "lucide-react"
 import { Link } from "react-router-dom"
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Card } from "./ui/card"
 
 function ExerciseTypeCard({ exerciseType }: { exerciseType: any }) {
   return (
     <Link to={`/type/${exerciseType._id}`}>
-      <Card className="flex h-24 w-44 transform-gpu flex-col border-2 border-double">
+      <Card className="flex h-full flex-col border-2 border-double active:translate-y-0.5">
         <div className=" flex justify-between">
           <div className="ml-1 mt-1 flex items-center gap-1 rounded-sm rounded-tl-lg border px-2">
-            <LucideTag fill="#cbd5e1" size={10} className="text-gray-500 dark:text-gray-400" />
-            <span className=" text-xs font-medium ">{exerciseType.type_session}</span>
+            <LucideTag fill="#cbd5e1" size={13} className="text-gray-500 dark:text-gray-400" />
+            <span className=" text-sm font-medium ">{exerciseType.type_session}</span>
           </div>
           <div className="mr-1 mt-1 flex items-center gap-1 rounded-sm rounded-tr-lg border px-2">
-            <LucideClock4 fill="#cbd5e1" size={10} className="text-gray-500 dark:text-gray-400" />
-            <span className=" text-xs font-medium ">{exerciseType.timer}</span>
+            <LucideClock4 fill="#cbd5e1" size={13} className="text-gray-500 dark:text-gray-400" />
+            <span className=" text-sm font-medium ">{exerciseType.timer}</span>
           </div>
         </div>
-        <div className="flex h-full flex-col justify-between px-3 py-2 ">
-          <div className=" text-sm font-black tracking-tighter	">{exerciseType.name}</div>
-          <div className=" border border-dashed border-gray-200 dark:border-gray-800"></div>
-          <div className=" flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
-            <LucideInfo className="" size={12} /> <div className="truncate text-xs">{exerciseType.advice}</div>
-          </div>
+        <div className="flex flex-col justify-center  px-3 py-2 ">
+          <div className=" text-lg font-black tracking-tighter	">{exerciseType.name}</div>
+          {exerciseType?.advice && (
+            <>
+              {" "}
+              <div className="my-1 flex w-1/2 rounded-full border border-gray-200 dark:border-gray-800"></div>
+              <div className="flex w-full items-center gap-1 text-gray-500 dark:text-gray-400">
+                <LucideInfo className=" flex-none" size={12} />
+                <div className=" truncate text-left text-xs">{exerciseType.advice}</div>
+              </div>
+            </>
+          )}
         </div>
       </Card>
     </Link>
