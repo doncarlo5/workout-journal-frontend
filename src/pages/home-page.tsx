d1a13e704b3cf7b25a8a81253b63ed222e54c39d
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-28 17:03:49 +0200

    Update home-page.tsx to improve UI layout and fix class names

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -11,9 +11,9 @@ export function HomePage() {
 
   console.log(user)
   return (
-    <div className="flex h-screen flex-col">
+    <div className="">
       <Navbar />
-      <main className="flex flex-1 items-center justify-center">
+      <main className="flex w-auto flex-1 items-center justify-center pb-24 pt-2 ">
         <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl/none">
@@ -22,12 +22,12 @@ export function HomePage() {
           </div>
           <div className="text-center">
             <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl">
-              Jette un oeil à tes exercices réalisés ou lance une nouvelle séance.
+              Créer un exercice, puis fait ta séance.{" "}
             </p>
           </div>
-          <div className=" flex max-w-sm flex-col justify-around gap-2  ">
+          <div className="flex w-screen flex-col justify-around gap-2 px-4  ">
             <Link
-              className="group flex w-full items-center justify-center rounded-lg bg-slate-100 px-2 py-3 shadow-lg  hover:text-teal-500 focus:text-teal-500 active:translate-y-0.5 active:shadow-inner"
+              className="group flex items-center justify-center rounded-lg bg-slate-100 px-2 py-3 shadow-lg  hover:text-teal-500 focus:text-teal-500 active:translate-y-0.5 active:shadow-inner"
               to="/history"
             >
               <div className=" flex items-center gap-4 pl-4">
@@ -39,8 +39,8 @@ export function HomePage() {
                   strokeWidth={1.5}
                 />
                 <p className="tab tab-whishlist block text-sm dark:text-gray-900  ">
-                  Créé un <span className=" font-bold"> exercice type </span> et personnalise-le avec un temps de repos,
-                  des répétitions et un conseil.
+                  Créé un <span className=" font-bold"> exercice</span>, ajoute un temps de repos, des répétitions et un
+                  conseil.
                 </p>
               </div>
               <div className="mx-4 h-8 w-[0.1rem] rounded-full bg-gray-200"></div>
@@ -59,18 +59,12 @@ export function HomePage() {
                       strokeWidth={1.5}
                     />
                     <p className="tab tab-whishlist block text-sm dark:text-gray-900">
-                      Réalise une <span className=" font-bold"> séance </span>{" "}
-                      <span className=" italic">Upper A/B</span> ou <span className=" italic">Lower</span> avec tes
-                      exercices types personnalisés.
+                      Réalise une <span className=" font-bold"> séance </span> <span className=" italic">Upper </span>{" "}
+                      ou <span className=" italic">Lower</span> avec tes exercices types personnalisés.
                     </p>
                   </div>
                   <div className="mx-4 h-8 w-[0.1rem] rounded-full bg-gray-200"></div>
-                  <LucideArrowRight
-                    className="inline-block group-hover:stroke-teal-500"
-                    color="rgb(107 114 128)"
-                    height={40}
-                    width={80}
-                  />
+                  <LucideArrowRight className="inline-block" color="rgb(107 114 128)" height={40} width={80} />
                 </div>
               }
             />
