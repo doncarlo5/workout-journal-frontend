0cf52fdf633d40ac1a7772a475b21aa086fe288a
doncarlos5 <pro.julien.thomas@gmail.com>
2024-05-01 21:29:10 +0200

    Refactor UI layout and update button text in welcome-page.tsx

--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -11,15 +11,15 @@ export function HomePage() {
     setTheme(theme === "light" ? "dark" : "light")
   }
   return (
-    <div className="flex h-screen flex-col">
+    <div className="">
       <Navbar />
-      <main className="flex flex-1 items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
+      <main className="container mx-auto my-0 flex h-dvh max-w-lg flex-col">
+        <div className="my-auto flex flex-col items-center justify-center">
           <div className="text-center">
             <h1 className="mb-2 text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl/none">Workout Journal</h1>
           </div>
 
-          {/* <div className="mt-3 flex gap-4">
+          {/* <div className="flex gap-4 mt-3">
             <Link className="w-[150px]" to="/signup">
               <Button className="w-full">Créer un compte</Button>
             </Link>
@@ -27,7 +27,7 @@ export function HomePage() {
           <div className="flex w-full justify-center pb-10">
             <div className="h-[0.05rem] w-1/4 rounded-lg bg-gray-900 dark:bg-gray-800"></div>
           </div>
-          <div className=" flex ">
+          <div className="flex ">
             <Button variant={"outline"} className="px-8 py-5" onClick={toggleTheme}>
               {theme === "light" ? (
                 <>
