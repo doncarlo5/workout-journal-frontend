78236e2debd315bbeafc8efac70190ae5eae0cf7
doncarlos5 <pro.julien.thomas@gmail.com>
2024-05-01 21:28:17 +0200

    Refactor UI layout and update button text in login-page.tsx

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -48,63 +48,63 @@ const LoginPage = () => {
 
   return (
     <>
-      <div className="mb-10 flex flex-col">
-        <Navbar />
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="mx-auto max-w-sm space-y-6">
-          <div className="space-y-2 text-center">
-            <h1 className="text-3xl font-bold">Se connecter 👤</h1>
-            <p className="text-gray-500 dark:text-gray-400">Connecte toi avec tes informations</p>
-          </div>
-          <div className="space-y-4">
-            <div className="flex flex-col gap-2">
-              <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  placeholder="johnny.bravo@email.com"
-                  value={formState.email}
-                  onChange={handleChange}
-                  required
-                  type="email"
-                />
+      <Navbar />
+      <main className="container mx-auto my-0 flex h-dvh max-w-lg flex-col">
+        <form onSubmit={handleSubmit}>
+          <div className="mx-auto max-w-sm space-y-6">
+            <div className="space-y-2 text-center">
+              <h1 className="text-3xl font-bold">Se connecter 👤</h1>
+              <p className="text-gray-500 dark:text-gray-400">Connecte toi avec tes informations</p>
+            </div>
+            <div className="space-y-4">
+              <div className="flex flex-col gap-2">
+                <div className="space-y-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    placeholder="johnny.bravo@email.com"
+                    value={formState.email}
+                    onChange={handleChange}
+                    required
+                    type="email"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="password">Mot de passe</Label>
+                  <Input
+                    id="password"
+                    placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
+                    value={formState.password}
+                    onChange={handleChange}
+                    required
+                    type="password"
+                  />
+                </div>
+                {error && (
+                  <Alert variant="destructive">
+                    <AlertTitle>Erreur</AlertTitle>
+                    <AlertDescription>{error}</AlertDescription>
+                  </Alert>
+                )}
               </div>
-              <div className="space-y-2">
-                <Label htmlFor="password">Mot de passe</Label>
-                <Input
-                  id="password"
-                  placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
-                  value={formState.password}
-                  onChange={handleChange}
-                  required
-                  type="password"
-                />
+              <Button className="w-full" type="submit">
+                Se connecter
+              </Button>
+              <div className="text-center text-sm">
+                Tu n'as pas de compte ?
+                <Link className="ml-1 underline hover:animate-pulse" to="/signup">
+                  Créer un compte
+                </Link>
               </div>
-              {error && (
-                <Alert variant="destructive">
-                  <AlertTitle>Erreur</AlertTitle>
-                  <AlertDescription>{error}</AlertDescription>
-                </Alert>
-              )}
-            </div>
-            <Button className="w-full" type="submit">
-              Se connecter
-            </Button>
-            <div className="text-center text-sm">
-              Tu n'as pas de compte ?
-              <Link className="ml-1 underline hover:animate-pulse" to="/signup">
-                Créer un compte
-              </Link>
-            </div>
-            <div>
-              <p>Info compte :</p>
-              <p>email: pro.julien.thomas</p>
-              <p>password: password</p>
+              <div>
+                <p>Info compte :</p>
+                <p>email: pro.julien.thomas</p>
+                <p>password: password</p>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </main>
     </>
   )
 }
