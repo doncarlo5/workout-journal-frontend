057adc62d1992dc5406104929fd0a9666b569c3d
doncarlos5 <pro.julien.thomas@gmail.com>
2024-05-01 21:29:07 +0200

    Refactor UI layout and update button text in trophy-page.tsx

--- src/pages/trophy-page.tsx
+++ src/pages/trophy-page.tsx
@@ -7,18 +7,18 @@ import TrophyComponent from "@/components/trophy-component"
 
 function TrophyPage() {
   return (
-    <div className="mx-auto  space-y-6 p-4">
+    <div className="">
       <div className="">
         <Navbar />
-        <main className="flex w-full flex-1 flex-col items-center justify-center">
-          <div className="flex items-center space-y-2 text-left">
+        <main className="container mx-auto my-0 flex h-dvh max-w-lg flex-col">
+          <div className="flex items-center pt-5">
             <Link to="/profile">
               <Button variant="outline" size="icon">
                 <ChevronLeft className="h-4 w-4" />
               </Button>
             </Link>
             <div>
-              <h1 className="ml-5 py-5 text-3xl font-medium">Trophées</h1>
+              <h1 className="ml-5 text-3xl font-medium">Trophées</h1>
             </div>
           </div>
 
