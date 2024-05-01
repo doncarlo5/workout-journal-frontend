26df2685e65d42bcffd1de998fc065c390e52f7e
doncarlos5 <pro.julien.thomas@gmail.com>
2024-05-01 21:28:52 +0200

    Refactor UI layout and add show/hide password functionality in signup-component.tsx

--- src/pages/signup-page.tsx
+++ src/pages/signup-page.tsx
@@ -21,6 +21,7 @@ const SignupPage = () => {
     email: "",
     password: "",
   })
+  const [showPassword, setShowPassword] = useState(false)
 
   const onTabChange = (tab: string) => {
     setTab(tab)
@@ -60,12 +61,12 @@ const SignupPage = () => {
   return (
     <div className="">
       <Navbar />
-      <main className="pb-24">
+      <main className="container mx-auto my-0 flex h-dvh max-w-lg flex-col">
         <Tabs
           value={tab}
           onValueChange={onTabChange}
           defaultValue={tab}
-          className="container flex flex-col items-center justify-center gap-4 px-10 pt-20 md:px-6"
+          className="my-auto flex flex-col items-center md:px-6"
         >
           <TabsList className="grid w-full max-w-2xl grid-cols-2 rounded-xl ">
             <TabsTrigger value="login">Connexion</TabsTrigger>
@@ -74,18 +75,27 @@ const SignupPage = () => {
           <TabsContent value="login">
             <>
               <form onSubmit={handleSubmit}>
-                <div className="mx-auto w-full space-y-6">
-                  <div className="mt-8 space-y-2 text-left">
+                <div className="">
+                  <div className="mt-5 text-left">
                     <h1 className="text-3xl font-bold">Se connecter</h1>
-                    <p className="text-gray-500 dark:text-gray-400">Connecte toi pour continuer.</p>
+                    <div className="items mb-5 flex items-baseline gap-1">
+                      <p className="text-gray-500 dark:text-gray-400">Tu n'as pas de compte?</p>
+                      <Button
+                        onClick={() => setTab("signup")}
+                        variant={"link"}
+                        className="m-0 p-0 text-gray-500 underline dark:text-gray-400"
+                      >
+                        Créer un compte
+                      </Button>
+                    </div>
                   </div>
                   <div className="space-y-4">
                     <div className="flex flex-col gap-2">
-                      <div className="space-y-2">
+                      <div className="space-y-1">
                         <Label htmlFor="email">Email</Label>
                         <Input
                           id="email"
-                          placeholder="Ton email"
+                          placeholder=""
                           value={formState.email}
                           onChange={handleChange}
                           required
@@ -96,13 +106,27 @@ const SignupPage = () => {
                         <Label htmlFor="password">Mot de passe</Label>
                         <Input
                           id="password"
-                          placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
+                          // placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
                           value={formState.password}
                           onChange={handleChange}
                           required
-                          type="password"
+                          type={showPassword ? "text" : "password"}
                           autoComplete="on"
                         />
+                      </div>
+                    </div>
+
+                    <div className="flex select-none items-center justify-between">
+                      <div className="flex items-center">
+                        <input
+                          id="showPassword"
+                          type="checkbox"
+                          className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                          onChange={() => setShowPassword(!showPassword)}
+                        />
+                        <label htmlFor="showPassword" className="ml-2 block text-sm text-gray-900">
+                          Afficher le mot de passe
+                        </label>
                       </div>
                     </div>
 
@@ -122,16 +146,6 @@ const SignupPage = () => {
                         <AlertDescription>{error}</AlertDescription>
                       </Alert>
                     )}
-                  </div>
-                  <div className="flex items-baseline gap-1 text-xs ">
-                    <p>Tu n'as pas encore de compte?</p>
-                    <Button
-                      onClick={() => setTab("signup")}
-                      variant={"link"}
-                      className=" flex items-center p-0 text-xs font-normal underline"
-                    >
-                      Créer un compte
-                    </Button>
                   </div>
                 </div>
               </form>
