90a7934269f065bc644e55e533233c35b4f1bd19
doncarlos5 <pro.julien.thomas@gmail.com>
2024-05-01 21:26:43 +0200

    Refactor UI layout and add functionality to dashboard-component.tsx, countdown-timer.tsx, body-weight-chart.tsx, and App.tsx Remove LucideClock4 import in new-type.tsx Add multiple colors and time intervals to countdown-timer.tsx Fix handleCheckboxChange function in one-session.tsx Add loading state to handleSubmit function in do-exercise-page.tsx Update TypesList component to change "Aucun type d'exercice" to "Aucun exercice" and update button text Add LucideClock4 component and improve UI layout in timer-page.tsx

--- src/components/exercise-card.tsx
+++ src/components/exercise-card.tsx
@@ -1,155 +1,138 @@
-import { LucideMessageSquareText } from "lucide-react"
+import { Edit } from "lucide-react"
 import { Link } from "react-router-dom"
-
-import { Badge } from "./ui/badge"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
 
 function ExerciseCard({ exercise }: { exercise: any }) {
   return (
-    <Link to={`/history/exercise/${exercise._id}`}>
-      <Card className="w-full max-w-sm">
-        <CardHeader className="px-4 pb-0 pt-4">
-          <CardTitle className="text-lg">{exercise.type.name}</CardTitle>
-        </CardHeader>
-        <CardContent className="ml-3 mt-2 px-1 pb-4">
-          <div className="mb-1 ml-1 flex items-center gap-4">
+    <Link
+      className="mb-4 flex w-56 rounded-2xl  bg-slate-100 px-3 py-2 shadow-md active:translate-y-0.5 active:shadow-none md:text-lg"
+      to={`/history/exercise/${exercise._id}`}
+    >
+      <div className="">
+        <p className="mb-2 ml-2 font-bold ">{exercise.type.name}</p>
+        <div className="flex justify-center">
+          <div className="flex gap-2">
+            <div className="flex flex-col gap-1 text-center">
+              <p className="pb-1 text-sm text-gray-500 ">Série</p>
+              <p className="flex items-center justify-center rounded-md bg-slate-200 bg-transparent px-3 py-1 font-mono text-sm text-gray-900 ">
+                1
+              </p>
+              <p className="flex items-center justify-center rounded-md bg-slate-200 bg-transparent px-3 py-1 font-mono text-sm text-gray-900 ">
+                2
+              </p>
+              <p className="flex items-center justify-center rounded-md bg-slate-200 bg-transparent px-3 py-1 font-mono text-sm text-gray-900 ">
+                3
+              </p>
+            </div>
+            <div className="flex flex-col gap-1 text-center">
+              <p className="pb-1 text-sm text-gray-500 ">Reps</p>
+              <p className="text-md flex h-7 w-12 items-center justify-center font-bold ">{exercise.rep[0]}</p>
+              <p className="text-md flex h-7 w-12 items-center justify-center font-bold">{exercise.rep[1]}</p>
+              <p className="text-md flex h-7 w-12 items-center justify-center font-bold ">{exercise.rep[2]}</p>
+            </div>
+
+            <div className="flex flex-col gap-1 text-center">
+              <p className="pb-1 text-sm text-gray-500 ">KG</p>
+              <p className="text-md flex h-7 w-12 items-center justify-center font-bold ">{exercise.weight[0]}</p>
+              <p className="text-md flex h-7 w-12 items-center justify-center font-bold ">{exercise.weight[1]}</p>
+              <p className="text-md flex h-7 w-12 items-center justify-center font-bold ">{exercise.weight[2]}</p>
+            </div>
+            <div className="flex flex-col gap-1 text-center">
+              <p className="pb-1 text-sm text-gray-500 ">{`[Range]`}</p>
+              <p className="flex h-7 w-12 items-center justify-center text-sm italic text-gray-700 ">
+                {exercise?.type.repRange1}
+              </p>
+
+              <p className="flex h-7 w-12 items-center justify-center text-sm italic text-gray-700 ">
+                {exercise?.type.repRange2}
+              </p>
+              <p className="flex h-7 w-12 items-center justify-center text-sm italic text-gray-700 ">
+                {exercise?.type.repRange3}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {exercise.comment && (
+          <div className="flex pl-2 pt-2 text-gray-500 dark:text-gray-400">
+            <div className="flex items-center gap-2">
+              <Edit className="size-4" /> <p className="text-sm ">{exercise.comment}</p>
+            </div>
+          </div>
+        )}
+      </div>
+
+      {/* <div className="flex justify-center py-4 rounded-2xl bg-slate-100 md:text-lg">
+        <div className="px-4 pt-4 pb-0">
+          <div className="text-lg">{exercise.type.name}</div>
+        </div>
+        <div className="px-1 pb-4 mt-2 ml-3">
+          <div className="flex items-center gap-4 mb-1 ml-1">
             <div className="flex w-28 items-baseline gap-0.5 font-bold sm:text-xl md:text-2xl">
               {exercise.rep[0]}
               <span className="text-xs font-normal text-gray-700"> REPS </span>{" "}
-              <span className=" ml-2 font-normal"> x</span>
+              <span className="ml-2 font-normal "> x</span>
             </div>
             <div className="flex w-40 items-baseline gap-0.5 font-bold sm:text-xl md:text-2xl">
               {exercise.weight[0]}
               <span className="text-xs font-normal text-gray-700"> KG</span>
             </div>
-            <div className="flex w-40 items-center text-sm font-medium sm:text-xl md:text-base">
+            <div className="flex items-center w-40 text-sm font-medium sm:text-xl md:text-base">
               <Badge
-                className=" min-h-10 min-w-8 select-none justify-center text-xs font-light sm:text-xl "
+                className="justify-center text-xs font-light select-none min-h-10 min-w-8 sm:text-xl"
                 variant="secondary"
               >
                 {exercise.type.repRange1}
               </Badge>
             </div>
           </div>
-          <div className="mb-1 ml-1 flex items-center gap-4">
+          <div className="flex items-center gap-4 mb-1 ml-1">
             <div className="flex w-28 items-baseline gap-0.5 font-bold sm:text-xl md:text-2xl">
               {exercise.rep[1]}
               <span className="text-xs font-normal text-gray-700"> REPS </span>{" "}
-              <span className=" ml-2 font-normal"> x</span>
+              <span className="ml-2 font-normal "> x</span>
             </div>
             <div className="flex w-40 items-baseline gap-0.5 font-bold sm:text-xl md:text-2xl">
               {exercise.weight[1]}
               <span className="text-xs font-normal text-gray-700"> KG</span>
             </div>
-            <div className="flex w-40 items-center text-sm font-medium sm:text-xl md:text-base">
+            <div className="flex items-center w-40 text-sm font-medium sm:text-xl md:text-base">
               <Badge
-                className=" min-h-10 min-w-8 select-none justify-center text-xs font-light sm:text-xl "
+                className="justify-center text-xs font-light select-none min-h-10 min-w-8 sm:text-xl"
                 variant="secondary"
               >
                 {exercise.type.repRange2}
               </Badge>
             </div>
           </div>
-          <div className="mb-1 ml-1 flex items-center gap-4">
+          <div className="flex items-center gap-4 mb-1 ml-1">
             <div className="flex w-28 items-baseline gap-0.5 font-bold sm:text-xl md:text-2xl">
               {exercise.rep[2]}
               <span className="text-xs font-normal text-gray-700"> REPS </span>{" "}
-              <span className=" ml-2 font-normal"> x</span>
+              <span className="ml-2 font-normal "> x</span>
             </div>
             <div className="flex w-40 items-baseline gap-0.5 font-bold sm:text-xl md:text-2xl">
               {exercise.weight[2]}
               <span className="text-xs font-normal text-gray-700"> KG</span>
             </div>
-            <div className="flex w-40 items-center text-sm font-medium sm:text-xl md:text-base">
+            <div className="flex items-center w-40 text-sm font-medium sm:text-xl md:text-base">
               <Badge
-                className=" min-h-10 min-w-8 select-none justify-center text-xs font-light sm:text-xl "
+                className="justify-center text-xs font-light select-none min-h-10 min-w-8 sm:text-xl"
                 variant="secondary"
               >
                 {exercise.type.repRange3}
               </Badge>
             </div>
           </div>
-        </CardContent>
+        </div>
         {exercise.comment && (
-          <CardContent className="border-t border-dotted px-4 pb-4 pt-3">
+          <div className="px-4 pt-3 pb-4 border-t border-dotted">
             <div className="flex items-center gap-2">
               <LucideMessageSquareText size={18} color="#64748B" />
               <CardDescription>{exercise.comment}</CardDescription>
             </div>
-          </CardContent>
-        )}
-      </Card>
-
-      {/* <Card className="transition ease-in-out hover:-translate-y-1 hover:shadow-lg">
-        <CardHeader className="  flex flex-row justify-between px-4 py-2 ">
-          <CardTitle className="flex items-center  ">{exercise.type.name}</CardTitle>
-          {exercise.comment && (
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <div className="flex transition hover:scale-110">
-                    <ChatBubbleIcon className="" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent>{exercise.comment}</TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          )}
-        </CardHeader>
-        <CardContent className=" m-1 p-1">
-          <div className="relative overflow-x-auto">
-            <table className="w-full rounded-md text-left">
-              <tbody className=" ">
-                <tr className=" bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
-                    <span className="text-2xl font-bold">{exercise.rep[0]}</span>{" "}
-                    <span className="text-sm text-gray-700">REPS</span>{" "}
-                  </td>
-                  <td className=" px-3 py-2">
-                    <span className=" text-2xl font-bold">{exercise.weight[0]}</span>{" "}
-                    <span className="text-sm text-gray-700">KG</span>
-                  </td>
-                  <td className=" rounded-xl bg-gray-300 px-3 py-2 text-center font-light tracking-tighter text-gray-700">
-                    {exercise.type.repRange1}
-                  </td>
-                </tr>
-
-                <tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
-                    <span className="text-2xl font-bold">{exercise.rep[1]}</span>{" "}
-                    <span className="text-sm text-gray-700">REPS</span>{" "}
-                  </td>
-                  <td className=" px-3 py-2">
-                    <span className=" text-2xl font-bold">{exercise.weight[1]}</span>{" "}
-                    <span className="text-sm text-gray-700">KG</span>
-                  </td>
-                  <td className="px-3 py-2 font-light tracking-tighter text-gray-700">
-                    {"("}
-                    {exercise.type.repRange2}
-                    {")"}
-                  </td>
-                </tr>
-
-                <tr className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                  <td className="border-r border-black border-r-slate-400 bg-clip-text px-3 py-2 ">
-                    <span className="text-2xl font-bold">{exercise.rep[2]}</span>{" "}
-                    <span className="text-sm text-gray-700">REPS</span>{" "}
-                  </td>
-                  <td className=" px-3 py-2">
-                    <span className=" text-2xl font-bold">{exercise.weight[2]}</span>{" "}
-                    <span className="text-sm text-gray-700">KG</span>
-                  </td>
-                  <td className="px-3 py-2 font-light tracking-tighter text-gray-700">
-                    {"("}
-                    {exercise.type.repRange3}
-                    {")"}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
           </div>
-        </CardContent>
-      </Card> */}
+        )}
+      </div> */}
     </Link>
   )
 }
