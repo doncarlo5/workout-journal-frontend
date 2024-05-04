878b2417fe3e75b4e9bc44e8338a56d121d7f5eb
doncarlos5 <pro.julien.thomas@gmail.com>
2024-05-04 08:03:44 +0200

    Refactor UI layout and update button text in welcome-page.tsx

--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -1,7 +1,7 @@
 import { Moon, Sun } from "lucide-react"
+import { Link } from "react-router-dom"
 
 import { Button } from "@/components/ui/button"
-import { Navbar } from "@/components/navbar"
 import { useTheme } from "@/components/theme-provider"
 
 export function HomePage() {
@@ -12,35 +12,34 @@ export function HomePage() {
   }
   return (
     <div className="">
-      <Navbar />
-      <main className="container mx-auto my-0 flex h-dvh max-w-lg flex-col">
+      <main className="container relative mx-auto my-0 flex h-dvh max-w-lg flex-col">
         <div className="my-auto flex flex-col items-center justify-center">
           <div className="text-center">
             <h1 className="mb-2 text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl/none">Workout Journal</h1>
           </div>
 
-          {/* <div className="flex gap-4 mt-3">
-            <Link className="w-[150px]" to="/signup">
-              <Button className="w-full">Créer un compte</Button>
-            </Link>
-          </div> */}
           <div className="flex w-full justify-center pb-10">
             <div className="h-[0.05rem] w-1/4 rounded-lg bg-gray-900 dark:bg-gray-800"></div>
           </div>
-          <div className="flex ">
-            <Button variant={"outline"} className="px-8 py-5" onClick={toggleTheme}>
-              {theme === "light" ? (
-                <>
-                  <Moon strokeWidth={1.1} size={30} />
-                </>
-              ) : (
-                <>
-                  <Sun strokeWidth={1.1} size={30} />
-                </>
-              )}
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </div>
+
+          <Link to="/signup">
+            <Button className="mt-5">Commencer</Button>
+          </Link>
+        </div>
+
+        <div className="absolute bottom-0 right-0 mb-8 mr-8">
+          <Button variant={"outline"} className="rounded-full px-4 py-5" onClick={toggleTheme}>
+            {theme === "light" ? (
+              <>
+                <Moon strokeWidth={1.1} size={28} />
+              </>
+            ) : (
+              <>
+                <Sun strokeWidth={1.1} size={28} />
+              </>
+            )}
+            <span className="sr-only">Toggle theme</span>
+          </Button>
         </div>
       </main>
     </div>
