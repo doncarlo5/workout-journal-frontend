24d0ad9d72bdce7bac07b52003bbbaf87a6a818a
Julien THOMAS <125746558+doncarlo5@users.noreply.github.com>
2024-05-16 17:54:26 +0200

    style: Update history page with French translations

--- src/pages/history-page.tsx
+++ src/pages/history-page.tsx
@@ -5,6 +5,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ExerciseComponent from "@/components/exercise-component"
 import { Navbar } from "@/components/navbar"
 import SessionComponent from "@/components/session-component"
+import NewSessionButton from "@/components/new-session-button"
+import { Plus } from "lucide-react"
 
 export function HistoryPage() {
   const [session, setSession] = useState([] as any[])
@@ -26,9 +28,11 @@ export function HistoryPage() {
   return (
     <div className="">
       <Navbar />
-      <main className="container mx-auto my-0 flex h-dvh max-w-md flex-col">
+      <main className="container mx-auto my-0 flex h-dvh max-w-lg flex-col">
         <div className="pt-10 ">
-          <h1 className="mb-5 text-4xl font-semibold tracking-tighter sm:text-5xl md:text-6xl/none">Mon historique.</h1>
+          <h1 className="mb-5 text-4xl font-semibold tracking-tighter sm:text-5xl md:text-6xl/none">
+            Mes séances.
+          </h1>
         </div>
         <Tabs defaultValue="session" className="">
           <TabsList className="grid grid-cols-2">
@@ -42,6 +46,15 @@ export function HistoryPage() {
             <ExerciseComponent />
           </TabsContent>
         </Tabs>
+        <NewSessionButton
+              Children={
+                <div className="fixed bottom-20 right-10 cursor-pointer">
+                  <div className="flex h-16 w-16 items-center justify-center rounded-full text-white shadow-lg focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-opacity-75 active:scale-95 active:shadow-inner">
+                    <Plus color="rgb(107 114 128)" className="inline-block " height={40} width={40} strokeWidth={1.5} />
+                  </div>
+                </div>
+              }
+            />
       </main>
     </div>
   )
