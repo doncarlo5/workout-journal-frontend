0b8ca3a627d001d43c6b2fb813d5c5efbfc6318c
Julien THOMAS <125746558+doncarlo5@users.noreply.github.com>
2024-05-17 09:29:57 +0200

    change routes for types

--- src/App.tsx
+++ src/App.tsx
@@ -28,33 +28,31 @@ function App() {
     <div className="flex-1 overflow-scroll">
       <ErrorBoundary>
         {/* <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> */}
-          <Routes>
-            <Route element={<IsNotAuthenticated />}>
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/login" element={<LoginPage />} />
-            </Route>
-            <Route element={<IsAuthenticated redirect={"/welcome"} />}>
-              <Route path="/" element={<HomePage />} />
-            </Route>
-            <Route path="/welcome" element={<WelcomePage />} />
-            <Route element={<IsAuthenticated redirect={"/"} />}>
-              <Route path="/history" element={<HistoryPage />} />
-              {/* <Route path="/sessions" element={<SessionsList />} /> */}
-              <Route path="/history/exercise/:exerciseId" element={<OneExercise />} />
-              <Route path="/history/session/:sessionId" element={<OneSession />} />
-              <Route path="/history/session/:sessionId/do-exercise" element={<DoExercisePage />} />
-              {/* <Route path="/exercises" element={<ExercicesList />} /> */}
-              <Route path="/type" element={<TypesList />} />
-              <Route path="/type/:typeId" element={<OneType />} />
-              <Route path="/type/new-type" element={<NewType />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/profile/stats" element={<StatsPage />} />
-              <Route path="/profile/trophy" element={<TrophyPage />} />
-              <Route path="/profile/timer" element={<TimerPage />} />
-              <Route path="/profile/settings" element={<SettingsPage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
+        <Routes>
+          <Route element={<IsNotAuthenticated />}>
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
+          <Route element={<IsAuthenticated redirect={"/welcome"} />}>
+            <Route path="/" element={<HomePage />} />
+          </Route>
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route element={<IsAuthenticated redirect={"/"} />}>
+            <Route path="/history" element={<HistoryPage />} />
+            <Route path="/history/exercise/:exerciseId" element={<OneExercise />} />
+            <Route path="/history/session/:sessionId" element={<OneSession />} />
+            <Route path="/history/session/:sessionId/do-exercise" element={<DoExercisePage />} />
+            <Route path="/profile/type" element={<TypesList />} />
+            <Route path="/profile/type/:typeId" element={<OneType />} />
+            <Route path="/profile/type/new-type" element={<NewType />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/profile/stats" element={<StatsPage />} />
+            <Route path="/profile/trophy" element={<TrophyPage />} />
+            <Route path="/profile/timer" element={<TimerPage />} />
+            <Route path="/profile/settings" element={<SettingsPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
         {/* </ThemeProvider> */}
       </ErrorBoundary>
     </div>
