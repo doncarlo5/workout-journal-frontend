627cb6afe37d63da97612716711e5172657498c2
Julien THOMAS <125746558+doncarlo5@users.noreply.github.com>
2024-05-22 09:13:10 +0200

    add skeleton loading

--- src/components/exercise-component.tsx
+++ src/components/exercise-component.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import { format } from "date-fns"
-import { LucideArrowRight, LucideLoader2, LucideMessageSquareText } from "lucide-react"
+import { LucideArrowRight, LucideMessageSquareText } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 
 import myApi from "@/lib/api-handler"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Skeleton } from "./ui/skeleton"
 
 export function ExerciseComponent() {
   const [exercise, setExercise] = useState([] as any[])
@@ -16,9 +17,10 @@ export function ExerciseComponent() {
     try {
       const response = await myApi.get("/api/exercise-user?limit=1000&sort=-updatedAt")
       setExercise(response.data)
-      setIsLoading(false)
     } catch (error) {
       console.error("Fetch error: ", error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -39,51 +41,75 @@ export function ExerciseComponent() {
 
   return (
     <div className="mt-5 flex flex-col items-center justify-center">
-      {!isLoading && exercise.length !== 0 && (
+      
         <div>
-          <Table className=" size-px">
-            <TableCaption>Tes exercices</TableCaption>
+          <Table className="size-px">
+            <TableCaption>{exercise.length} exercices.</TableCaption>
             <TableHeader>
               <TableRow>
                 <TableHead></TableHead>
                 <TableHead>Date</TableHead>
                 <TableHead>Nom</TableHead>
                 <TableHead>Série</TableHead>
-
                 <TableHead></TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {exercise.map((exercise) => (
-                <TableRow className="group cursor-pointer" onClick={() => handleLink(exercise._id)} key={exercise._id}>
-                  <TableCell>
-                    <div>
-                      {" "}
-                      {exercise.comment ? (
-                        <LucideMessageSquareText size={16} />
-                      ) : (
-                        <LucideMessageSquareText className=" text-slate-200" size={16} />
-                      )}
-                    </div>
-                  </TableCell>
-                  <TableCell>{formatDate(exercise.session.date_session)}</TableCell>
-                  <TableCell>{exercise.type?.name}</TableCell>
-                  <TableCell>
-                    {exercise.rep[0]}x{exercise.weight[0]}
-                    <br />
-                    {exercise.rep[1]}x{exercise.weight[1]}
-                    <br />
-                    {exercise.rep[2]}x{exercise.weight[2]}
-                  </TableCell>
-                  <TableCell className="items-center justify-center">
-                    <LucideArrowRight className=" text-slate-300 group-hover:text-slate-900" size={18} />{" "}
-                  </TableCell>
-                </TableRow>
-              ))}
+              {isLoading
+                ? 
+                  Array.from({ length: 7 }).map((_, index) => (
+                    <TableRow key={index}>
+                      <TableCell>
+                        <Skeleton className="h-4 w-4 rounded" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-20" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-12 w-[4.5rem]" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-16 w-16" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-4" />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                : 
+                  exercise.map((exercise) => (
+                    <TableRow
+                      className="group cursor-pointer"
+                      onClick={() => handleLink(exercise._id)}
+                      key={exercise._id}
+                    >
+                      <TableCell>
+                        <div>
+                          {exercise.comment ? (
+                            <LucideMessageSquareText size={16} />
+                          ) : (
+                            <LucideMessageSquareText className="text-slate-200" size={16} />
+                          )}
+                        </div>
+                      </TableCell>
+                      <TableCell>{formatDate(exercise.session.date_session)}</TableCell>
+                      <TableCell>{exercise.type?.name}</TableCell>
+                      <TableCell>
+                        {exercise.rep[0]}x{exercise.weight[0]}
+                        <br />
+                        {exercise.rep[1]}x{exercise.weight[1]}
+                        <br />
+                        {exercise.rep[2]}x{exercise.weight[2]}
+                      </TableCell>
+                      <TableCell className="items-center justify-center">
+                        <LucideArrowRight className="text-slate-300 group-hover:text-slate-900" size={18} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
             </TableBody>
           </Table>
         </div>
-      )}
+      
       {!isLoading && exercise.length === 0 && (
         <div className="flex flex-1 items-center justify-center">
           <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
@@ -91,13 +117,6 @@ export function ExerciseComponent() {
               <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl">Aucun exercice.</p>
             </div>
             <div className="flex flex-col gap-4 "></div>
-          </div>
-        </div>
-      )}
-      {isLoading && (
-        <div className="container flex flex-col items-center justify-center p-20">
-          <div className="">
-            <LucideLoader2 className=" animate-spin" size={32} />
           </div>
         </div>
       )}
--- src/components/session-component.tsx
+++ src/components/session-component.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
-import { LucideArrowRight, LucideLoader2, LucideMessageSquareText } from "lucide-react"
+import { LucideArrowRight, LucideMessageSquareText } from "lucide-react"
 import { useNavigate } from "react-router-dom"
 
 import myApi from "@/lib/api-handler"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
+import { Skeleton } from "./ui/skeleton"
 
 export function SessionComponent() {
   const [session, setSession] = useState([] as any[])
@@ -15,9 +17,10 @@ export function SessionComponent() {
     try {
       const response = await myApi.get("/api/sessions?limit=1000&sort=-date_session")
       setSession(response.data)
-      setIsLoading(false)
     } catch (error) {
       console.error("Fetch error: ", error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -35,57 +38,77 @@ export function SessionComponent() {
 
   return (
     <>
-      {" "}
-      {!isLoading && session.length !== 0 && (
-        <div className="mt-5 flex flex-col items-center justify-center pb-5 ">
-          <div>
-            <Table>
-              <TableCaption>Liste de tes séances passées</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead></TableHead>
-                  <TableHead></TableHead>
-                  <TableHead>Date</TableHead>
-                  <TableHead>Type</TableHead>
-                  <TableHead>Nbr</TableHead>
-                  <TableHead></TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {session.map((oneSession) => (
-                  <TableRow
-                    className="group cursor-pointer"
-                    onClick={() => handleLink(oneSession._id)}
-                    key={oneSession._id}
-                  >
-                    <TableCell>
-                      <div
-                        className={`me-2 h-2.5 w-2.5 rounded-full ${oneSession.is_done ? "bg-green-500" : "bg-red-500"}`}
-                      ></div>
-                    </TableCell>
-                    <TableCell>
-                      <div>
-                        {" "}
-                        {oneSession.comment ? (
-                          <LucideMessageSquareText size={16} />
-                        ) : (
-                          <LucideMessageSquareText className=" text-slate-200" size={16} />
-                        )}
-                      </div>
-                    </TableCell>
-                    <TableCell>{formatDate(oneSession.date_session)}</TableCell>
-                    <TableCell>{oneSession.type_session}</TableCell>
-                    <TableCell className="text-center ">{oneSession.exercise_user_list.length}</TableCell>
-                    <TableCell className="w-10 items-center justify-center">
-                      <LucideArrowRight className=" text-slate-300 group-hover:text-slate-900" size={18} />{" "}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
+      <div className="mt-5 flex flex-col items-center justify-center pb-5 ">
+        <div>
+          <Table>
+            <TableCaption>Liste de tes séances passées</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead></TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead>Type</TableHead>
+                <TableHead>Nbr</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {isLoading
+                ? Array.from({ length: 15 }).map((_, index) => (
+                    <TableRow key={index}>
+                      <TableCell>
+                        <Skeleton className="me-2 h-2.5 w-2.5 rounded-full" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-4 rounded" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-20" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-16" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-8" />
+                      </TableCell>
+                      <TableCell>
+                        <Skeleton className="h-4 w-4" />
+                      </TableCell>
+                    </TableRow>
+                  ))
+                : session.map((oneSession) => (
+                    <TableRow
+                      className="group cursor-pointer"
+                      onClick={() => handleLink(oneSession._id)}
+                      key={oneSession._id}
+                    >
+                      <TableCell>
+                        <div
+                          className={`me-2 h-2.5 w-2.5 rounded-full ${oneSession.is_done ? "bg-green-500" : "bg-red-500"}`}
+                        ></div>
+                      </TableCell>
+                      <TableCell>
+                        <div>
+                          {oneSession.comment ? (
+                            <LucideMessageSquareText size={16} />
+                          ) : (
+                            <LucideMessageSquareText className="text-slate-200" size={16} />
+                          )}
+                        </div>
+                      </TableCell>
+                      <TableCell>{formatDate(oneSession.date_session)}</TableCell>
+                      <TableCell>{oneSession.type_session}</TableCell>
+                      <TableCell className="text-center ">{oneSession.exercise_user_list.length}</TableCell>
+                      <TableCell className="w-10 items-center justify-center">
+                        <LucideArrowRight className="text-slate-300 group-hover:text-slate-900" size={18} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+            </TableBody>
+          </Table>
         </div>
-      )}
+      </div>
+
       {!isLoading && session.length === 0 && (
         <div className="flex flex-1 items-center justify-center">
           <div className="container flex flex-col items-center justify-center gap-4 px-4 md:px-6">
@@ -93,15 +116,6 @@ export function SessionComponent() {
               <p className="max-w-[600px] py-5 text-gray-500 dark:text-gray-400 md:text-xl">Aucune séance.</p>
             </div>
             <div className="flex flex-col gap-4 "></div>
-          </div>
-        </div>
-      )}
-      {isLoading && (
-        <div className="flex flex-1 items-center justify-center">
-          <div className="container flex flex-col items-center justify-center p-20">
-            <div className="">
-              <LucideLoader2 className=" animate-spin" size={32} />
-            </div>
           </div>
         </div>
       )}
