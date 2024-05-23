01d14f4ab3c3f4cb73382710b2a3f1fcf6101f0b
Julien THOMAS <125746558+doncarlo5@users.noreply.github.com>
2024-05-23 21:25:18 +0200

    add filter to exercise type

--- src/components/type-component.tsx
+++ src/components/type-component.tsx
@@ -9,15 +9,28 @@ import ExerciseTypeCard from "./exercise-type-card"
 
 export function TypeComponent() {
   const [exerciseType, setExerciseType] = useState([] as any[])
+  const [filteredExerciseType, setFilteredExerciseType] = useState([] as any[])
   const [isLoading, setIsLoading] = useState(true)
+  const [currentFilter, setCurrentFilter] = useState<string | null>(null)
 
   const fetchTypes = async () => {
     try {
       const response = await myApi.get("/api/exercise-type?limit=1000&sort=-updatedAt")
       setExerciseType(response.data)
+      setFilteredExerciseType(response.data)
       setIsLoading(false)
     } catch (error) {
       console.error("Fetch error: ", error)
+      setIsLoading(false)
+    }
+  }
+
+  const handleFilter = (typeSession: string | null) => {
+    setCurrentFilter(typeSession)
+    if (typeSession) {
+      setFilteredExerciseType(exerciseType.filter((type) => type.type_session === typeSession))
+    } else {
+      setFilteredExerciseType(exerciseType)
     }
   }
 
@@ -37,9 +50,8 @@ export function TypeComponent() {
           <div className="flex justify-center">
             <Link to={`/profile/type/new-type`}>
               <Button className="mx-auto">
-                {" "}
                 <PlusIcon className="mr-2 size-5" />
-                Créer un exercice{" "}
+                Créer un exercice
               </Button>
             </Link>
           </div>
@@ -47,7 +59,7 @@ export function TypeComponent() {
             <div className="pb-10 ">
               <div className="">
                 <div className="rounded-lg border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-gray-950 dark:shadow-sm">
-                  <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+                  <div className="mb-4 grid grid-cols-1 gap-4 ">
                     {isLoading ? (
                       <div className="col-span-full flex items-center justify-center">
                         <div className="container flex flex-col items-center justify-center p-20">
@@ -57,9 +69,46 @@ export function TypeComponent() {
                         </div>
                       </div>
                     ) : (
-                      exerciseType.map((exerciseType: any) => (
-                        <ExerciseTypeCard exerciseType={exerciseType} key={exerciseType._id} />
-                      ))
+                      <div className=" mt-5">
+                        <div className="mb-4 flex  justify-center">
+                          <div className="flex gap-3 ">
+                            <Button
+                              variant="secondary"
+                              className={`w-1/4 border-transparent border-2 ${currentFilter === "Upper A" ? "box-border  border-2 border-slate-400 " : ""}`}
+                              onClick={() => handleFilter("Upper A")}
+                            >
+                              Upper A
+                            </Button>
+                            <Button
+                              variant="secondary"
+                              className={`w-1/4 border-transparent border-2 ${currentFilter === "Lower" ? "box-border  border-2 border-slate-400" : ""}`}
+                              onClick={() => handleFilter("Lower")}
+                            >
+                              Lower
+                            </Button>
+                            <Button
+                              variant="secondary"
+                              className={`w-1/4 border-transparent border-2 ${currentFilter === "Upper B" ? "box-border  border-2 border-slate-400" : ""}`}
+                              onClick={() => handleFilter("Upper B")}
+                            >
+                              Upper B
+                            </Button>
+                            <Button
+                              variant="secondary"
+                              className={`w-1/4 border-transparent border-2 ${currentFilter === null ? "box-border  border-2 border-slate-400" : ""}`}
+                              onClick={() => handleFilter(null)}
+                            >
+                              All
+                            </Button>
+                          </div>
+                        </div>
+
+                        <div className="flex flex-col gap-4">
+                          {filteredExerciseType.map((exerciseType: any) => (
+                            <ExerciseTypeCard exerciseType={exerciseType} key={exerciseType._id} />
+                          ))}
+                        </div>
+                      </div>
                     )}
                   </div>
                 </div>
